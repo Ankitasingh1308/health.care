edac5735710069a07fccf2bce5e110c6d6835553
Ankitasingh1308 <136838487+Ankitasingh1308@users.noreply.github.com>
2025-06-13 18:24:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // bas App.js import ho raha
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
